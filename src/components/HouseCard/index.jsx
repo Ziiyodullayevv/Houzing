32136349f8c621780c92fe295b9b15f17f4ccca0
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-06-14 18:32:45 +0300

    house-card finished

--- src/components/HouseCard/index.jsx
+++ src/components/HouseCard/index.jsx
@@ -2,36 +2,60 @@ import React from "react";
 import { Container, Content, Details, Footer, Icons, Img } from "./style";
 import noimg from "../../assets/img/noimg.jpeg";
 
-const HouseCard = ({ url, title }) => {
+const HouseCard = ({
+  url,
+  title,
+  bed,
+  bath,
+  garage,
+  ruler,
+  info,
+  subtitle,
+}) => {
   return (
     <Container>
       <Img src={url || noimg} />
       <Content>
         <div className="subTitle">{title || "New Appartment Nice Wiew"}</div>
-        <div className="info">Quincy St, Brooklyn, NY, USA</div>
+        <div className="info">{info || "Quincy St, Brooklyn, NY, USA"}</div>
         <Details>
           <Details.Item>
             <Icons.Bed />
-            <div className="info">Bed 23</div>
+            <div className="info">Bed {bed || 0}</div>
           </Details.Item>
 
           <Details.Item>
             <Icons.Bath />
-            <div className="info">Bath 34</div>
+            <div className="info">Bath {bath || 0}</div>
           </Details.Item>
 
           <Details.Item>
             <Icons.Garage />
-            <div className="info">Garage 45</div>
+            <div className="info">Garage {garage || 0}</div>
           </Details.Item>
 
           <Details.Item>
             <Icons.Ruler />
-            <div className="info">Ruler 51</div>
+            <div className="info">Ruler {ruler || 0}</div>
           </Details.Item>
         </Details>
       </Content>
-      <Footer></Footer>
+      <Footer>
+        <Details>
+          <Details.Item>
+            <div className="info">$28000/mo</div>
+            <div className="subTitle">$7.500/mo</div>
+          </Details.Item>
+          <Details style={{ gap: "1rem" }}>
+            <div className="icon-style">
+              <Icons.Resize />
+            </div>
+            <div className="icon-style">
+              <Icons.Love />
+            </div>
+          </Details>
+        </Details>
+      </Footer>
     </Container>
   );
 };
--- src/components/HouseCard/style.js
+++ src/components/HouseCard/style.js
@@ -3,15 +3,21 @@ import { ReactComponent as bed } from "../../assets/icons/bed.svg";
 import { ReactComponent as bath } from "../../assets/icons/bath.svg";
 import { ReactComponent as garage } from "../../assets/icons/car.svg";
 import { ReactComponent as ruler } from "../../assets/icons/ruler.svg";
+import { ReactComponent as love } from "../../assets/icons/love.svg";
+import { ReactComponent as resize } from "../../assets/icons/resize.svg";
 
 const Container = styled.div`
   width: 380px;
-  height: 430px;
-  filter: drop-shadow(0px 20px 38px rgba(0, 0, 0, 0.06))
-    drop-shadow(0px 7px 46px rgba(0, 0, 0, 0.06))
-    drop-shadow(0px 8px 15px rgba(0, 0, 0, 0.06));
-  border: 1px solid black;
+  border-radius: 4px;
+  height: auto;
+  border: 1px solid #e6e9ec;
   background-color: white;
+  transition: all 0.1s ease-in;
+  &:hover {
+    filter: drop-shadow(0px 20px 38px rgba(0, 0, 0, 0.06))
+      drop-shadow(0px 7px 46px rgba(0, 0, 0, 0.06))
+      drop-shadow(0px 8px 15px rgba(0, 0, 0, 0.06));
+  }
 `;
 
 const Img = styled.img`
@@ -27,7 +33,7 @@ const Details = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 16px 0;
+  margin: 14px 0 8px;
 `;
 
 Details.Item = styled.div`
@@ -43,9 +49,19 @@ Icons.Bed = styled(bed)``;
 Icons.Bath = styled(bath)``;
 Icons.Garage = styled(garage)``;
 Icons.Ruler = styled(ruler)``;
+Icons.Love = styled(love)`
+  width: 15px;
+  height: 15px;
+`;
+Icons.Resize = styled(resize)`
+  width: 15px;
+  height: 15px;
+`;
 
 const Footer = styled.div`
+  margin-top: 16px;
   border-top: 1px solid #e6e9ec;
+  padding: 0 20px;
 `;
 
 export { Container, Img, Content, Details, Icons, Footer };
