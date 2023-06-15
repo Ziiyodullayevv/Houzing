28899c8804323faeca64a6247baeacc8da4e6265
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-06-15 21:43:49 +0300

    started https

--- src/components/Properties/index.jsx
+++ src/components/Properties/index.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container } from "./style";
 
 const Properties = () => {
-  return <div>Properties</div>;
+  const { REACT_APP_BASE_URL: url } = process.env;
+  console.log(url);
+
+  useEffect(() => {
+    fetch(`${url}/houses/list`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  });
+
+  return <Container></Container>;
 };
 
 export default Properties;
