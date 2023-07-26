0063ac73e2f96865c9dd715d4670e21985f7a886
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-07-26 17:49:55 +0500

    bug fixed

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient();
 
 root.render(
-  <React.StrictMode>
+  <>
     <QueryClientProvider client={queryClient}>
       <RootContext>
         <Root />
       </RootContext>
     </QueryClientProvider>
-  </React.StrictMode>
+  </>
 );
